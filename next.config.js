97217d2d55947fce1697fb8051e1255e9a8ee813
devmasson <developermasson@gmail.com>
2025-12-16 21:53:57 -0300

    feat: add auto refresh web

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Configuração para otimização
+
   swcMinify: true,
-  // Configuração de imagens
+
   images: {
     domains: [],
-    unoptimized: true, // Para permitir SVGs locais
+    unoptimized: true, 
+  },
+
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+
+      config.watchOptions = {
+        poll: 1000, // Verifica mudanças a cada segundo
+        aggregateTimeout: 300, // Aguarda 300ms antes de recompilar
+      };
+    }
+    return config;
   },
 }
 
